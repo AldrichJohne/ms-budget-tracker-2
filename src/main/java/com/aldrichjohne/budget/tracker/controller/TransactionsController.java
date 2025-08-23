292f2e765665abca1f9cdfc345f7759b8a539d43
AldrichJohne <aldrichjohneramosulabo@gmail.com>
2025-08-22 23:54:11 -0400

    updated transaction controller
    add/update transaction controller

--- src/main/java/com/aldrichjohne/budget/tracker/controller/TransactionsController.java
+++ src/main/java/com/aldrichjohne/budget/tracker/controller/TransactionsController.java
@@ -1,8 +1,14 @@
 package com.aldrichjohne.budget.tracker.controller;
 
+import com.aldrichjohne.budget.tracker.exception.customexceptions.InputIsNullException;
+import com.aldrichjohne.budget.tracker.exception.customexceptions.InsufficientBalanceException;
+import com.aldrichjohne.budget.tracker.exception.customexceptions.MandatoryInputMissingException;
 import com.aldrichjohne.budget.tracker.model.entity.dto.InflowDTO;
+import com.aldrichjohne.budget.tracker.model.entity.dto.OutFlowDTO;
 import com.aldrichjohne.budget.tracker.service.InFlowService;
+import com.aldrichjohne.budget.tracker.service.OutFlowService;
 import com.aldrichjohne.budget.tracker.util.mapper.model.ResponseWrapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,13 +19,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/txn/v1")
 public class TransactionsController {
     private final InFlowService inFlowService;
+    private final OutFlowService outFlowService;
 
-    public TransactionsController(InFlowService inFlowService) {
+    public TransactionsController(
+            InFlowService inFlowService,
+            OutFlowService outFlowService) {
         this.inFlowService = inFlowService;
+        this.outFlowService = outFlowService;
     }
 
-    @PostMapping
+    @PostMapping("/in")
     public ResponseEntity<ResponseWrapper> addCash(@RequestBody InflowDTO inflowDTO) throws IllegalAccessException {
         return ResponseEntity.ok(inFlowService.addCash(inflowDTO));
+    }
+
+    @PostMapping("/bill")
+    public ResponseEntity<ResponseWrapper> payBill(@RequestBody OutFlowDTO outFlowDTO)
+            throws MandatoryInputMissingException,
+            InputIsNullException,
+            InsufficientBalanceException,
+            IllegalAccessException {
+        return ResponseEntity.ok(outFlowService.payBill(outFlowDTO));
     }
 }
